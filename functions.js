0e29d05263a472c7ede00f8d8cb79df7cf58df22
CYTeo <86589539+CYTeo@users.noreply.github.com>
2023-12-06 18:46:35 +0800

    completing design and setup js function

---
+++ functions.js
@@ -0,0 +1,19 @@
+// function to receive upload of the audio mp3 file
+
+// function to control the play/pause of the audio
+const handlingVideoPlay = () => {};
+
+// function to control the volume
+const handlingSoundVolume = () => {};
+
+// function to control the progress bar
+const handlingProgressTimebar = () => {};
+
+// function to control the time display
+const handlingTime = () => {};
+
+// function to mute the sound
+const mute = () => {};
+
+// function to handle error - when file not found or loading errors
+const errorHandling = () => {};
